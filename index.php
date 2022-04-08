e14e4b9258e33d5172e7e92e8dc741d86f68f969
Mohammad Habib <mohmad.habib@gmail.com>
2022-04-08 22:57:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>MKNH - Coming Soon</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="shortcut icon" type="image/png" href="fav.png"/>
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<head>
+  <title>MKNH (Coming Soon)</title>
+  <style>@import url(/assets/style.css)</style>
+</head>
+
+<div id=container>
+  <img src="https://pbs.twimg.com/profile_images/735541433730486272/i8R_x7jI_400x400.jpg"" id="avatar" width="250" draggable="false"/>
+
+  <div id=text>
+    <h1>Building…</h1>
+    <p title="(I guess)">(coming soon)</p>
+  </div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
